ccc081e2763a2c359c636147fad6923074938e56
ygor-sk <igor.inas@gmail.com>
2018-09-13 20:30:03 +0200

    added TestUtils

--- src/test/scala/sk/ygor/codility/TestUtils.scala
+++ src/test/scala/sk/ygor/codility/TestUtils.scala
@@ -3,4 +3,6 @@ package sk.ygor.codility
 trait TestUtils {
 
   def formatArray(array: Array[_]): String = if (array.length > 100) "[too long]" else array.mkString("[", ", ", "]")
+
+  def formatString(s: String): String = if (s.length > 100) "[too long]" else s
 }
