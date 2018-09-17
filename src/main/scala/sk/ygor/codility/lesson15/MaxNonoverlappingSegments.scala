0a547c82f89cffe71030502391f327abd49ac999
ygor-sk <igor.inas@gmail.com>
2018-09-17 14:29:31 +0200

    Lession15 MaxNonoverlappingSegments

---
+++ src/main/scala/sk/ygor/codility/lesson15/MaxNonoverlappingSegments.scala
@@ -0,0 +1,22 @@
+package sk.ygor.codility.lesson15
+
+
+class MaxNonoverlappingSegments {
+
+  def solution(a: Array[Int], b: Array[Int]): Int = {
+    val segments = a.zip(b).map { case (from, to) => Segment(from, to) }
+    var result = 0
+    var lastEnd = -1
+    segments.foreach(segment => {
+      if (segment.from > lastEnd) {
+        result +=1
+        lastEnd = segment.to
+      }
+    })
+    result
+  }
+
+
+  case class Segment(from: Int, to: Int)
+
+}
